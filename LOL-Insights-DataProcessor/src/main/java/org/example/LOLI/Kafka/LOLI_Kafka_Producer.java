326b0aa800b9f51cf2f73698b862ce02b067cb39
a-derr <austinderrick15@gmail.com>
2025-02-13 12:34:18 -0600

    Initialized branch for kafka-ingestion
    Outlined steps for creating kafka producer for LOLI (LOL insights)

--- LOL-Insights-DataProcessor/src/main/java/org/example/LOLI/Kafka/LOLI_Kafka_Producer.java
+++ LOL-Insights-DataProcessor/src/main/java/org/example/LOLI/Kafka/LOLI_Kafka_Producer.java
@@ -1,4 +1,19 @@
 package org.example.LOLI.Kafka;
 
 public class LOLI_Kafka_Producer {
+    public static void main(String[] args) {
+        //ingest data from Riot Games Live Client Data API
+
+        //organize data into topics for kafka ingestion
+            // Game_events - Kills, Objectives, item purchases
+            // player_stats - CS, K/D/A, gold, ability cooldowns
+            // team_info - objectives taken, turrets destroyed
+
+        // Format data in JSON and serialize messages
+            // Makes processing more efficient
+
+        // Send messages to kafka
+    }
 }
+
+
